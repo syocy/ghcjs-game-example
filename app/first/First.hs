53e07a3fa7faff4eb194b454c3673ddad2b9bc69
syocy <osmium.k@gmail.com>
2017-04-18 11:10:21 +0900

    fix

--- app/first/First.hs
+++ app/first/First.hs
@@ -1,26 +1,8 @@
 module Main where
 
-import GHCJS.Types (JSVal, JSString)
-import qualified Data.JSString as JSS
-import Language.Javascript.JSaddle
-import Control.Lens
-import qualified Data.Text as T
-
-foreign import javascript unsafe "console.log($1)" consoleLog :: JSString -> IO ()
-
-consoleLog2 arr = jsg ("console" :: String) >>= \c -> c ^. jsf ("log" :: String) arr
-
-consoleLog3 str = jsgf ("console.log" :: String) [str]
+(><) :: String -> String -> String
+a >< b = a ++ "x" ++ b
 
 main :: IO ()
 main = do
-  putStrLn "hellob"
-  consoleLog $ JSS.pack "hello2"
-  runJSaddle () $ do
-    consoleLog2 ["hello", "hello", "hello"]
-    consoleLog2 ()
-    consoleLog2 (1::Int,"aa")
-    -- consoleLog3 "hello2"
-    consoleLog $ JSS.pack "2111"
-  return ()
-
+  putStrLn $ "GHC" >< "JS"
